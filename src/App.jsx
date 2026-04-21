fe19845fe1546615bc129fcaa00f0f72d77ffb73
Victor Dorado <88350091+CerberoDgvm@users.noreply.github.com>
2026-04-21 15:18:38 -0500

    fix: resolver componente Zigzag

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Hero      from './components/Hero'
 import About     from './components/About'
 import Projects  from './components/Projects'
 import Contact   from './components/Contact'
-import ZigZag    from './components/ZigZag'
+import ZigZag    from './components/Zigzag'
 
 function App() {
   return (
